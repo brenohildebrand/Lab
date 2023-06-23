2fc5de7e6914f130788db28b1875e421fa745811
Breno Hildebrand <hello@brenohildebrand.com>
2023-06-23 11:07:32 -0300

    Yay!

--- error.py
+++ error.py
@@ -1,12 +1,5 @@
-import sys
-
 try:
-    f = open('myfile.txt')
-    s = f.readline()
-    i = int(s.strip())
-except OSError as err:
-    print("OS Error:", err)
-except ValueError:
-    print("Could not convert data to an integer.")
-except Exception as err:
-    print(f"Unexpected {err=}, {type(err)=}")
+    raise
+except RuntimeError:
+    print('RuntimeError')
+    raise
