827676d2b02e0cba730587524c4b3dface80f1c3
Breno Hildebrand <hello@brenohildebrand.com>
2023-06-23 11:01:16 -0300

    Ok

--- error.py
+++ error.py
@@ -1,18 +1,12 @@
-class B(Exception):
-    pass
+import sys
 
-class C(B):
-    pass
-
-class D(C):
-    pass
-
-for cls in [B, C, D]:
-    try:
-        raise cls()
-    # except D:
-    #     print("D")
-    except C:
-        print("C")
-    except B:
-        print("B")
+try:
+    f = open('myfile.txt')
+    s = f.readline()
+    i = int(s.strip())
+except OSError as err:
+    print("OS Error:", err)
+except ValueError:
+    print("Could not convert data to an integer.")
+except Exception as err:
+    print(f"Unexpected {err=}, {type(err)=}")
