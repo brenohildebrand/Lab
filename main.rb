5ccdb4d3ad236a42f7684b28dec469f924f63e0a
Breno Hildebrand <hello@brenohildebrand.com>
2023-06-26 16:03:24 -0300

    Again and again

--- main.rb
+++ main.rb
@@ -1,52 +1,11 @@
-class Sequence
-    include Enumerable # include the methods of this module in this class
-
-    def initialize(from, to, by)
-        @from, @to, @by = from, to, by
-    end
-
-    def each
-        x = @from
-        while x <= @to
+module Sequences
+    def self.fromtoby(from, to, by)
+        x = from
+        while x <= to
             yield x
-            x += @by
+            x += by
         end
-    end
-
-    def length
-        return 0 if @from > @to
-        Integer((@to - @from) / @by) + 1
-    end
-
-    # define another name for the same method
-    alias size length # size is now a synonym for length
-
-    # override the array-access operator
-    def[](index)
-        return nil if index < 0
-        v = @from + index * @by
-        if v <= @to
-            v
-        else
-            nil
-        end
-    end
-
-    # override arithmetic operators to return new Sequence objects
-    def *(factor)
-        Sequence.new(@from*factor, @to*factor, @by*factor)
-    end
-
-    def +(offset)
-        Sequence.new(@from+offset, @to+offset, @by)
     end
 end
 
-s = Sequence.new(1, 10, 2)
-s.each { |x| print x }
-print "\n"
-print s[s.size - 1]
-print "\n"
-t = (s+1) * 2
-t.each { |x| print x }
-print "\n"
+Sequences.fromtoby(1, 10, 2) {|x| puts x}
