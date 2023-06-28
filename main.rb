5cc2fada6b79642c4fddae526d6677b27fcff6f7
Breno Hildebrand <hello@brenohildebrand.com>
2023-06-27 21:53:16 -0300

    Hm...

--- main.rb
+++ main.rb
@@ -1,11 +1,2 @@
-class ListChanger
-    def change_list(list)
-        list.push(0)
-    end
-end
-
-changer = ListChanger.new
-array = [1, 2, 3]
-
-changer.change_list(array)
-print array
+require "zlib"
+puts Zlib::Inflate.inflate(STDIN.read)
